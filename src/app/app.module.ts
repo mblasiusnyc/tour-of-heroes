faf08b99903f633031efe82561e49fa5213dd269
Michael Blasius <mblasiusnyc@gmail.com>
2017-05-27 19:41:45 -0600

    Refactored routing into separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { RouterModule }   from '@angular/router';
+import { AppRoutingModule }		 from './app-routing.module';
 
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }     from './dashboard.component';
+import { DashboardComponent }  from './dashboard.component';
+
 
 import { HeroService }         from './hero.service';
 
@@ -14,25 +16,7 @@ import { HeroService }         from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-      	path: 'detail/:id',
-      	component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
