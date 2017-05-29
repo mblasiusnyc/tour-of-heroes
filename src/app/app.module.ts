346f0972dbe33d63632474eae6d2c046223b29f2
Michael Blasius <mblasiusnyc@gmail.com>
2017-05-29 17:49:55 -0600

    Data is now being loaded from mock server/db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
 import { AppRoutingModule }		 from './app-routing.module';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { DashboardComponent }  from './dashboard.component';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { HeroService }         from './hero.service';
 
@@ -16,6 +19,8 @@ import { HeroService }         from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
