dfeef9fe98faacb1c17777e70e90d4b2af34c0c3
Michael Blasius <mblasiusnyc@gmail.com>
2017-05-29 23:07:22 -0600

    Heroes can now be updated

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,10 +10,6 @@ export class HeroService {
 	private heroesUrl = 'api/heroes';  // URL to web api
 	constructor(private http: Http) { }
 
-  // getHeroes(): Promise<Hero[]> {
-  // 	return Promise.resolve(HEROES);
-  // }
-
   getHeroes(): Promise<Hero[]> {
   	return this.http.get(this.heroesUrl)
   		.toPromise()
@@ -26,9 +22,22 @@ export class HeroService {
   	return Promise.reject(error.message || error);
   }
 
-
   getHero(id: number): Promise<Hero> {
-  	return this.getHeroes()
-  		.then(heroes => heroes.find(hero => hero.id === id));
+  	const url = `${this.heroesUrl}/${id}`;
+  	return this.http.get(url)
+  		.toPromise()
+  		.then(response => response.json().data as Hero)
+  		.catch(this.handleError);
+  }
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  update(hero: Hero): Promise<Hero> {
+  	const url = `${this.heroesUrl}/${hero.id}`;
+  	return this.http
+  		.put(url, JSON.stringify(hero), {headers: this.headers})
+  		.toPromise()
+  		.then(() => hero)
+  		.catch(this.handleError);
   }
 }
