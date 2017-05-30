d96e3b9134ca08ae44bd46bca08eb3615f7657f9
Michael Blasius <mblasiusnyc@gmail.com>
2017-05-29 23:23:07 -0600

    Users can now add and delete heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -32,12 +32,29 @@ export class HeroService {
 
   private headers = new Headers({'Content-Type': 'application/json'});
 
+  create(name: string): Promise<Hero> {
+  	return this.http
+  		.post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+  		.toPromise()
+  		.then(res => res.json().data as Hero)
+  		.catch(this.handleError)
+  }
+
   update(hero: Hero): Promise<Hero> {
   	const url = `${this.heroesUrl}/${hero.id}`;
   	return this.http
   		.put(url, JSON.stringify(hero), {headers: this.headers})
   		.toPromise()
   		.then(() => hero)
+  		.catch(this.handleError);
+  }
+
+  delete(id: number): Promise<Hero> {
+  	const url = `${this.heroesUrl}/${id}`
+  	return this.http
+  		.delete(url, {headers: this.headers})
+  		.toPromise()
+  		.then(() => null)
   		.catch(this.handleError);
   }
 }
